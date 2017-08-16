0d0fd0c5fc78191240c148ea493dfca1c8225697
Estudiantes <Estudiantes@labing50602.udistrital.edu.co>
2017-08-16 18:01:53 -0500

    TRABAJO

---
+++ Sumador2/src/sumador2/Sumador2.java
@@ -0,0 +1,20 @@
+
+package sumador2;
+
+
+public class Sumador2 {
+
+ ventana numero1;
+ numero1 new ventana
+    
+    public static void main(String[] args) {
+      
+       Ventana miVentana=new Ventana();
+       
+       numero1.numero2
+        
+        
+    }
+    
+    
+}
