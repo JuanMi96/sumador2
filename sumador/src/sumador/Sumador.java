396052f2a911fea7b73d8e235dbc58bcf03ec635
Daniel <Daniel@DESKTOP-79T5FQV>
2017-08-17 22:31:29 -0500

    parejas2

---
+++ sumador/src/sumador/Sumador.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sumador;
+
+
+public class Sumador {
+
+ int numero1;
+ int numero2;
+ 
+ int suma(){
+     
+     return( numero1 + numero2);
+     
+ }
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
